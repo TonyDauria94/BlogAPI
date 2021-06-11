f45d60488eb0999ba0f8e5d4769d331700944f12
TonyDauria94 <tonydauria94@gmail.com>
2021-06-11 16:43:25 +0200

    Aggiunto l'httpStatus.

--- src/main/java/it/rdev/blog/api/controller/CategoriaController.java
+++ src/main/java/it/rdev/blog/api/controller/CategoriaController.java
@@ -3,8 +3,11 @@ package it.rdev.blog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.rdev.blog.api.controller.dto.CategoriaDTO;
@@ -17,12 +20,21 @@ public class CategoriaController {
 	private BlogCategoriaDetailsService categoryService;
 	
 	@RequestMapping(path = "/api/categoria", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public List<CategoriaDTO> get() {
 		
 		List<CategoriaDTO> list = categoryService.findAll();
 		
+		if(list.isEmpty()) 
+			throw new RuntimeException("Non sono presenti categorie.");
+		
 		return list;
 	}
 	
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public String handleRuntimeException(RuntimeException ex) {
+		return ex.getMessage();
+	}
 	
 }
--- src/main/java/it/rdev/blog/api/controller/TagController.java
+++ src/main/java/it/rdev/blog/api/controller/TagController.java
@@ -3,8 +3,11 @@ package it.rdev.blog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.rdev.blog.api.controller.dto.TagDTO;
@@ -17,11 +20,21 @@ public class TagController {
 	private BlogTagDetailsService tagService;
 
 	@RequestMapping(path = "/api/tag", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public List<TagDTO> get() {
 		
 		List<TagDTO> list = tagService.findAll();
 		
+		if(list.isEmpty()) 
+			throw new RuntimeException("Non sono presenti tags.");
+		
 		return list;
 	}
+
 	
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public String handleRuntimeException(RuntimeException ex) {
+		return ex.getMessage();
+	}
 }
